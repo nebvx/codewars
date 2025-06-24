1aec2923c7be64ca142bcb93e435b34287362364
nebu <126800543+nebvx@users.noreply.github.com>
2025-06-24 15:30:10 +0200

    expanding the base functionalities

--- pokerhand.cpp
+++ pokerhand.cpp
@@ -1,6 +1,32 @@
 #include <vector>
 #include <string>
 
+struct Card {
+  public:
+  enum Suit {Spades, Hearts, Diamonds, Clubs};
+  
+  private:
+  int value;
+  Suit suit;
+  
+  static std::map<char, int> cardValueMap;
+  static std::map<char, Suit> cardSuitMap;
+
+  public:
+  Card (const std::string card) {
+    //value
+    std::string value_str = card.substr(0);
+    char c = value_str[0];
+    value = cardValueMap[c];
+    
+    //Suit
+    value_str = card.substr(1);
+    c = value_str[0];
+    suit = cardSuitMap[c];
+
+  }
+};
+
 struct PokerHand {
   private:
   std::vector<Card> pokerhand;
@@ -9,44 +35,30 @@ struct PokerHand {
   PokerHand (const char* pokerhand_unsorted){
     std::string hand = pokerhand_unsorted;
     for (size_t i {0}; i < hand.size(); i += 3){
-      char card = std::string::substr(i, 2);
+      std::string card = hand.substr(i, 2);
       pokerhand.push_back(Card(card));
     }
   }
+  
+  int getFirstcard() {
+    auto card = pokerhand[0];
+  }
 };
 
-struct Card {
-  private:
-  int value;
-  enum Suit {Spades, Hearts, Diamonds, Clubs}
-  Suit suit;
-  
-  static const std::map<char, int> cardValueMap;
-  static const std::map<char, Suit> cardSuitMap;
-
-  public:
-  Card (const char c) {
-    //value
-    value = cardValueMap[std::string::substr(0)];
-    //Suit
-    suit = cardSuitMap[std::string::substr(1)];
-  }
-}
-
-const std::map<char, int> Card::cardValueMap = {
+std::map<char, int> Card::cardValueMap = {
   {'2', 2}, {'3', 3}, {'4', 4}, {'5', 5}, {'6', 6}, {'7', 7}, {'8', 8}, {'9', 9}, 
   {'T', 10}, {'J', 11}, {'Q', 12}, {'K', 13}, {'A', 14}
 };
 
-const std::map<char, Suit> cardSuitMap = {
-  {'S', Suit::Spades}, {'H', Suit::Hearts}, {'D', Suit::Diamonds}, {'C', Suit::Clubs}
+std::map<char, Card::Suit> cardSuitMap = {
+  {'S', Card::Suit::Spades}, {'H', Card::Suit::Hearts}, {'D', Card::Suit::Diamonds}, {'C', Card::Suit::Clubs}
 };
 
 
 enum class Result { Win, Loss, Tie };
 
 Result compare (const PokerHand &player, const PokerHand &opponent) {
-  
+  std::cout << "player: " << player.getFirstcard(); 
   
   return Result::Loss;
 }
