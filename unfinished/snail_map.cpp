30d4040cf66de10d1fdb117449196b29912375c3
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-04 23:17:28 +0200

    started to implement top, down, left and right

--- unfinished/snail_map.cpp
+++ unfinished/snail_map.cpp
@@ -3,9 +3,26 @@
 std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
   std::vector<int> ret;
   size_t top{0}, right{snail_map[0].size()}, down{right}, left{0};
-  for (auto i : snail_map){
+  while (top < down){
+    //top
+    for (size_t i {0}; i < right; ++i){
+      ret.push_back(snail_map[top][i]);
+    }
+    ++top;
+    //right
+    for (size_t i {0}; i < down - 1; ++i){
+      ret.push_back(snail_map[top + i][right]);
+    }
     
-    ret.push_back(i);
+    //down 
+    for (size_t i {down}; i > 0; --i){
+      ret.push_back(snail_map[down][i]);
+    }
+ 
+    //left
+    for (size_t i {0}; i < ; ++i){
+      ret.push_back(snail_map[top + i][right]);
+    }
   }
   
   return ret;
