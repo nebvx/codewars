5b683ae1ed6eb83616c9b05cda487691ce82d4ba
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-05 00:25:41 +0200

    fixed all remaining bugs & moved to finished

--- finished/snail_map.cpp
+++ finished/snail_map.cpp
@@ -9,29 +9,32 @@ std::vector<int> snail(const std::vector<std::vector<int>> &snail_map) {
   size_t top{0}, right{snail_map[0].size()}, down{right}, left{0};
   while (top < down){
     //top
-    for (size_t i {0}; i < right; ++i){
+    for (size_t i {left}; i < right; ++i){
       ret.push_back(snail_map[top][i]);
+      std::cout << "top: " << snail_map[top][i] << "\n";
+      
     }
     ++top;
-    //right
     
+    //right
     for (size_t i {top}; i < right; ++i){
-      std::cout << "i: " << i << ", right: " << right << "\n";
-      ret.push_back(snail_map[i][right - 1]);
+    ret.push_back(snail_map[i][right - 1]);
+    std::cout << "right: " << snail_map[i][right - 1] << "\n";
     }
     --right;
-    
+
     //down 
-    for (size_t i {right}; i > 0; --i){
-      std::cout << "i: " << i << ", right: " << right << "\n";
+    for (size_t i {right}; i > left; --i){
       ret.push_back(snail_map[down - 1][i - 1]);
+      std::cout << "down: " << snail_map[down - 1][i - 1] << "\n";
+
     }
     --down;
-    
-    
+
     //left
-    for (size_t i {0}; i < down - top; ++i){
-      ret.push_back(snail_map[down - i][left]);
+    for (size_t i {down}; i > top; --i){
+      ret.push_back(snail_map[i - 1][left]);
+      std::cout << "left: " << snail_map[i - 1][left] << "\n";
     }
     ++left;
   }
