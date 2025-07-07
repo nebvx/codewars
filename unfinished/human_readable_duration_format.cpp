8d0ac52d68094e4fda2224619a38d51b76f5a220
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-07 20:24:07 +0200

    translating int to string

--- unfinished/human_readable_duration_format.cpp
+++ unfinished/human_readable_duration_format.cpp
@@ -2,16 +2,24 @@
 
 std::string format_duration(int seconds) {
   if (seconds == 0) { return "now"; }
-  
-  int hours {second}, temp {second};
-  for (size_t h {seconds}; h > 60; h/60){
+  int hours {0}, min {0}, sec {0};
+  int temp {second};
+  for (; temp > 3600; temp/60){
     ++hours;
   }
-  
-  int minutes = seconds
-  for (size_t h {seconds}; h < 60; h/60){
-    ++hours;
+  for (; temp > 60; h/60){
+    ++min;
+  } 
+  for (; temp > 0; --h){
+    ++sec;
   } 
   
-  return "now";
+  //int to string
+  string ret = "";
+  if (hours != 0) {
+    ret += std::to_string(hours) + " hour";
+    if (hours > 1) { ret += "s"; }
+  }
+  if 
+  return ;
 }
