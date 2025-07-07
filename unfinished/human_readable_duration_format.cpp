4800d9db7d6baedca665cf93f481d049decc51b9
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-08 00:23:09 +0200

    added description

--- unfinished/human_readable_duration_format.cpp
+++ unfinished/human_readable_duration_format.cpp
@@ -1,3 +1,8 @@
+/*
+Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+*/
+
 #include <string>
 
 std::string format_duration(int seconds) {
