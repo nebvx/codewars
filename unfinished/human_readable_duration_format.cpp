2ecd6e13ec5e174cb88fd325dc88ca6063aab891
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-08 00:22:25 +0200

    finished it

--- unfinished/human_readable_duration_format.cpp
+++ unfinished/human_readable_duration_format.cpp
@@ -2,24 +2,69 @@
 
 std::string format_duration(int seconds) {
   if (seconds == 0) { return "now"; }
-  int hours {0}, min {0}, sec {0};
-  int temp {second};
-  for (; temp > 3600; temp/60){
-    ++hours;
-  }
-  for (; temp > 60; h/60){
-    ++min;
-  } 
-  for (; temp > 0; --h){
-    ++sec;
+  int year {0}, day {0}, hour {0}, min {0}, sec {0};
+  for (int temp {seconds}; temp > 0; ){
+    if (temp >= 31536000){
+      ++year;
+      temp -= 31536000;
+    } else if (temp >= 86400) {
+      ++day;
+      temp -= 86400;
+    } else if (temp >= 3600){
+      ++hour;
+      temp -= 3600;
+    } else if (temp >= 60) {
+      ++min;
+      temp -= 60;
+    } else {
+      ++sec;
+      --temp;
+    }
   } 
   
   //int to string
-  string ret = "";
-  if (hours != 0) {
-    ret += std::to_string(hours) + " hour";
-    if (hours > 1) { ret += "s"; }
+  std::string time = "";
+  if (year != 0){
+    time += std::to_string(year) + " year";
+    if (year > 1) { 
+      time += "s"; 
+    }
   }
-  if 
-  return ;
+  if (day != 0){
+    if (!time.empty()) {
+      sec && min && day == 0 ? time += " and " : time += ", ";
+    }
+    time += std::to_string(day) + " day";
+    if (day > 1) { 
+      time += "s"; 
+    }
+  }
+  if (hour != 0) {
+    if (!time.empty()) {
+      sec && min == 0 ? time += " and " : time += ", ";
+    }
+    time += std::to_string(hour) + " hour";
+    if (hour > 1) { 
+      time += "s"; 
+    }
+  }
+  if (min != 0) {
+    if (!time.empty()) {
+      sec == 0 ? time += " and " : time += ", ";
+    }
+    time += std::to_string(min) + " minute";
+    if (min > 1) { 
+      time += "s"; 
+    }
+  }
+  if (sec != 0) {
+    if (!time.empty()) {
+      time += " and ";
+    }
+    time += std::to_string(sec) + " second";
+    if (sec > 1) { 
+      time += "s"; 
+    }
+  }
+  return time;
 }
