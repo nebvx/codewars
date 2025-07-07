f455a07972a2b9647c4bcfa60543b39ff756f828
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-07 20:17:22 +0200

    buildingl loops for hours and minutes

--- unfinished/human_readable_duration_format.cpp
+++ unfinished/human_readable_duration_format.cpp
@@ -1,5 +1,17 @@
 #include <string>
 
 std::string format_duration(int seconds) {
-  // your code here
+  if (seconds == 0) { return "now"; }
+  
+  int hours {second}, temp {second};
+  for (size_t h {seconds}; h > 60; h/60){
+    ++hours;
+  }
+  
+  int minutes = seconds
+  for (size_t h {seconds}; h < 60; h/60){
+    ++hours;
+  } 
+  
+  return "now";
 }
