f3425ce75984e9db2b6aa775283ef76df83a0f74
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-08 12:30:16 +0200

    started with the implementation of finding the ship

--- unfinished/battleship_validator.cpp
+++ unfinished/battleship_validator.cpp
@@ -1,7 +1,18 @@
 #include <vector>
-bool validate_battlefield(std::vector<std::vector<int>> field) {
-  // Write your magic here ;)
-  
-  
+bool validate_battlefield(std::vector< std::vector<int> > field) {
+  std::vector<std::vector<bool>> visited(10, std::vector<bool>(10, false));
+  for (size_t y {0}; y < 10; ++y){
+    for (size_t x {0}; x < 10; ++x){
+      if (field[y][x] == 1){ //ship found
+        for (size_t i {0}; i < 4; ++i){
+          if (field[y + i][x]) { //down 
+            
+          } else if (field[y][x + i]) { // right
+            
+          }
+        }
+      }
+    }
+  }
   return true;
 }
