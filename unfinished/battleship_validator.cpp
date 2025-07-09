20e83dbf4c2a38509beca8b1da59b34d76b9216f
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-09 20:52:29 +0200

    seperated couting the ship and finding overlaps

--- unfinished/battleship_validator.cpp
+++ unfinished/battleship_validator.cpp
@@ -4,41 +4,54 @@ bool validate_battlefield(std::vector< std::vector<int> > field) {
   std::vector<std::vector<bool>> visited(10, std::vector<bool>(10, false));
   size_t battleship {0}, cruiser {0}, destroyer {2}, sub{0};
   //single battleship (size of 4 cells), 2 cruisers (size 3), 3 destroyers (size 2) and 4 submarines (size 1).
+  bool vertical {true};
   for (size_t y {0}; y < 10; ++y){
     for (size_t x {0}; x < 10; ++x){
-      if (field[y][x] == 1){ //ship found
-        size_t ship_size {0};
-        for (size_t i {1}; i < 4; ++i){ //ship size loop
-          bool down {true}, right {true};
-          if (field[y + i][x] && down) { //down 
-            right = false;
-            if (right) { return false; } //overlap
-            ++ship_size;
-          } else if (field[y][x + i] && right) { // right
-            down = false;
-            if (down) { return false; } //overlap
-            ++ship_size;
-          } else { //0 in both directions
-            break;
+      //vertical
+      if (vertical) {
+        if (field[y][x] == 1){ //found a ship
+          size_t ship_size {0};
+          for (size_t i {0}; i < 4; ++i){ // finding ship size 
+            ++ship_size
+            if (field[y][x + i] == 0){
+              break;
+            }  
           }
+          for (size_t i {0}; i < ship_size; ++i){ //looking for unallowed overlaps
+            //above
+            if (!y == 0) { //making sure it's not the first line
+                
+            }
+            //beneath
+            if (!y == 9){
+            }
+          }
+          
         }
-        //ship_size gets saved
-        switch (ship_size) {
-          case 1:
-            if (sub > 4) { return false; } //only four allowed
-            ++sub;
-          case 2:
-            if (destroyer > 3) { return false; } //only three allowed
-            ++destroyer;
-          case 3:
-            if (cruiser > 2) { return false; } //only two allowed
-            ++cruiser;
-          case 4:
-            if (battleship > 1) { return false; } //only one allowed
-            ++battleship;
-        } 
+        
+        if (x == 9) { //reached last line, switching to horizontal
+          vertical = false;
+          x = 0;
+        }
       }
-      
+      if (!vertical) { //horizontal
+        
+        
+      }
+      switch (ship_size) {
+        case 1:
+          if (sub > 4) { return false; } //only four allowed
+          ++sub;
+        case 2:
+          if (destroyer > 3) { return false; } //only three allowed
+          ++destroyer;
+        case 3:
+          if (cruiser > 2) { return false; } //only two allowed
+          ++cruiser;
+        case 4:
+          if (battleship > 1) { return false; } //only one allowed
+          ++battleship;
+      } 
     }
   }
   return true;
