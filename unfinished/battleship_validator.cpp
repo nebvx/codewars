46ea2e17a5e44543b28913dfdfbd5f665972bae5
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-09 18:40:12 +0200

    completed first (and buggy) implementation of the ship count loop

--- unfinished/battleship_validator.cpp
+++ unfinished/battleship_validator.cpp
@@ -1,17 +1,31 @@
 #include <vector>
 bool validate_battlefield(std::vector< std::vector<int> > field) {
+  // Write your magic here ;)
   std::vector<std::vector<bool>> visited(10, std::vector<bool>(10, false));
+  size_t battleship {0}, cruiser {0}, destroyer {2}, sub{0};
+  //single battleship (size of 4 cells), 2 cruisers (size 3), 3 destroyers (size 2) and 4 submarines (size 1).
   for (size_t y {0}; y < 10; ++y){
     for (size_t x {0}; x < 10; ++x){
       if (field[y][x] == 1){ //ship found
-        for (size_t i {0}; i < 4; ++i){
-          if (field[y + i][x]) { //down 
-            
-          } else if (field[y][x + i]) { // right
-            
+        size_t ship_size {0};
+        for (size_t i {0}; i < 4; ++i){ //ship size loop
+          bool down {true}, right {true};
+          if (field[y + i][x] && down) { //down 
+            right = false;
+            if right { return false; } //overlap
+            ++ship_size;
+          } else if (field[y][x + i] && right) { // right
+            down = false;
+            if (down) { return false; } //overlap
+            ++ship_size;
+          } else { //0 in both directions
+            break;
           }
         }
+        //ship_size gets saved
+        switch
       }
+      
     }
   }
   return true;
