b12dc2739343bd6e99e2b5f108a6c6bb4c09a7e4
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-10 18:09:47 +0200

    add kata of rail fence cipher encoding and decoding

---
+++ unfinished/rail_fence_cipher.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+std::string encode_rail_fence_cipher(const std::string &str, int n) {
+  return "";
+}
+
+std::string decode_rail_fence_cipher(const std::string &str, int n) {
+  return "";
+}
