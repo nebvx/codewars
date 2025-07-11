838bd742e56e386459bf1443b7d71735fc4c2b92
nebu <126800543+nebvx@users.noreply.github.com>
2025-07-11 16:46:21 +0200

    add implementation for encode - only first line functions

--- unfinished/rail_fence_cipher.cpp
+++ unfinished/rail_fence_cipher.cpp
@@ -1,7 +1,22 @@
 #include <string>
 
 std::string encode_rail_fence_cipher(const std::string &str, int n) {
-  return "";
+  std::string encoded = "", messages = str;
+  int count {0};
+  for (int i {n - 1}; i >= 0; --i) {
+    std::string temp = "";
+    for (size_t j {0}; j < messages.size(); ++j){
+      if (count++ == 0) {
+          encoded += messages[j];
+      } else {
+          temp += messages[j];
+      }
+      if (count == i + i) { count = 0; }
+    }
+    messages = temp;
+    std::cout << "ended: " << i << ", messages: " << messages << "\n";
+  }
+  return encoded;
 }
 
 std::string decode_rail_fence_cipher(const std::string &str, int n) {
