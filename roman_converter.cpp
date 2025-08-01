2c2b4fe41b609ec01d31ba68fe30e3b4349d6fc0
Nebu <berndorfer.lena@gmail.com>
2025-08-01 23:07:08 +0200

    added new kata

---
+++ roman_converter.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+class RomanHelper{
+  public:
+    std::string to_roman(unsigned int n){
+      return "";
+    }
+    int from_roman(std::string rn){
+      return 0;
+    }
+} RomanNumerals;
