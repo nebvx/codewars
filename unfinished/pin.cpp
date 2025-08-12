e65a67aef6080d492f16db7fc9149d14ee44126b
nebu <126800543+nebvx@users.noreply.github.com>
2025-08-12 14:43:56 +0200

    Update implementation to handle single-digit input correctly

--- unfinished/pin.cpp
+++ unfinished/pin.cpp
@@ -2,5 +2,22 @@
 #include <vector>
 
 std::vector<std::string> get_pins(std::string observed) {
-  return {};
+    std::vector<std::string> res;
+    int num {std::stoi(observed)};
+    while (num > 0) {
+        int dig = num % 10;
+        num /= 10;
+        res.push_back(std::to_string(dig));
+        if (dig != 1 && dig != 4 && dig != 7 && dig != 0) res.push_back(std::to_string(dig - 1));
+        if (dig != 9 && dig != 6 && dig != 3 && dig != 0) res.push_back(std::to_string(dig + 1));
+
+        if (dig > 3) { res.push_back(std::to_string(dig - 3)); }
+        if (dig < 7) { res.push_back(std::to_string(dig + 3)); } 
+        
+        //expection; mostly with 0
+        if (dig == 8) { res.push_back("0"); }
+    }
+
+    std::sort(res.begin(), res.end());
+    return res;
 }
